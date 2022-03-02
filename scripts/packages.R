5e33a12d3bf4d4e3e27a07844753bc0a3804e9ba
Deborah Ho <debho@umich.edu>
2022-03-02 11:20:55 -0500

    initial commit

---
+++ scripts/packages.R
@@ -0,0 +1,10 @@
+##############################################################################
+############### Maternal care effects on offspring personality ###############
+##############################################################################
+### How does maternal care style influence the development of offspring
+### personality and what are the fitness effects on offspring?
+### offspring personality ~ maternal care * density + other stuff
+
+##############################################################################
+### Script for loading in packages
+
